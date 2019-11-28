8178f7f8049b6da6dee82fd1c1a67668e646d6ab
« <emmajouffroypro@gmail.com>
2019-11-28 12:35:10 -0500

    almost finished notebook

--- preprocessing_data/preprocessing.py
+++ preprocessing_data/preprocessing.py
@@ -4,7 +4,12 @@ from preprocessing_data.get_data import *
 
 
 def information_dataset(data):
-    data.sample(frac=0.7).head()
+    classe_1 = data['sepal_length'].loc[data['species'] == "virginica"]
+    classe_2 = data['sepal_length'].loc[data['species'] == "virginica"]
+    print("Forme du jeu de données adult_data.csv : ", data.shape)
+    print("nombre de personnes dans la classe 1", classe_1.shape )
+    print("nombre de personnes dans la classe 2", classe_2.shape)
+    print(data.sample(frac=0.7).head())
     return
 
 if __name__ == '__main__':
